cef55487f5d841b1201dd02af921eb646790df80
Thibault-GX <thibault.grimoux@gmail.com>
2019-12-19 14:39:20 +0100

    Légère optimisation du code après une discussion avec Anthony et ajout de commentaires.

--- TP/index.php
+++ TP/index.php
@@ -3,15 +3,28 @@ $partNb = 9;
 $exerciseNb = 'TP';
 include '../header.php';
 setlocale(LC_TIME, 'fr_FR.utf8', 'fra');
-$earliestYear = 1900;
-$chosenMonthDays = '';
-$firstDay = '';
+// The earliest year selectable will be 200 years in the past from the current one
+$earliestYear = date('Y') - 200;
+// The furthest year selectable will be 50 years in the futur from the current one
+$furthestYear = date('Y') + 50;
+// Default month and year will be the current ones
+$month = date('n');
+$year = date('Y');
 $months = ['', 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'];
+// If the month has been specified through the form, $month will become this value
 if (isset($_GET['month'])) {
     $month = $_GET['month'];
-} else {
-    $month = date('n');
 }
+// If the year has been specified through the form, $year will become this value
+if (isset($_GET['year'])) {
+    $year = $_GET['year'];
+}
+// Define the number of days in the month chosen by the user
+$chosenMonthDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
+// Define a string with the first day of the month, for instance: 'tuesday 1 september 1992'
+$firstDay = strftime('%A %e %B %Y', strtotime('01-' . $month . '-' . $year));
+// Create an array which begin from 1 and extend to the number of days defined by $chosenMonthDays
+$monthLength = range(1, $chosenMonthDays);
 ?>
 <!-- Form to select the month and the year you want -->
 <form method="get" action="index.php">
@@ -32,7 +45,7 @@ if (isset($_GET['month'])) {
     <?php
     // Create a <select> with an option for each year between the current one and the one defined in the variable $earliestYear
     echo ('<select name="year">');
-    foreach (range(date('Y'), $earliestYear) as $userChoice) {
+    foreach (range($furthestYear, $earliestYear) as $userChoice) {
         echo ('<option value="' . $userChoice . '">' . $userChoice . '</option>');
     }
     echo ('</select>');
@@ -40,11 +53,7 @@ if (isset($_GET['month'])) {
     <input type="submit" value="C'est parti !"/>
 </form>
 <div class="w-100 p-2"></div>
-<h2><?php
-    if ((isset($_GET['month'])) & (isset($_GET['year']))) {
-        echo ($months[$month] . ' ' . $_GET['year']);
-    }
-    ?></h2>
+<h2><?php echo ($months[$month] . ' ' . $year); ?></h2>
 <div class="container-fluid rounded bg-secondary flex-column justify-content-around">
     <div class="ml-5 mr-1 mt-3 row">
         <div class="dayLabel text-center"><p>Lundi</p></div>
@@ -57,34 +66,23 @@ if (isset($_GET['month'])) {
     </div>
     <div class="ml-5 mr-1 mb-3 row">
         <?php
-        // If the month and the year have been transmitted through the form
-        if ((isset($_GET['month'])) & (isset($_GET['year']))) {
-            // Define the number of days in the month chosen by the user
-            $chosenMonthDays = cal_days_in_month(CAL_GREGORIAN, $_GET['month'], $_GET['year']);
-            // Define a string with the first day of the month, for instance: 'tuesday 1 september 1992'
-            $firstDay = strftime('%A %e %B %Y', strtotime('01-' . $_GET['month'] . '-' . $_GET['year']));
-            // Become the value of the month defined by the user
-            $month = $_GET['month'];
-            // Create an array which begin from 1 and extend to the number of days defined by $chosenMonthDays
-            $monthLength = range(1, $chosenMonthDays);
 // Create empty cells, regarding the name of the first day detected in $firstDay
-            if (strpos($firstDay, 'mardi') !== false) {
-                echo '<div class="emptyCell text-center"></div>';
-            } elseif (strpos($firstDay, 'mercredi') !== false) {
-                echo '<div class="emptyCell text-center"></div><div class="emptyCell text-center"></div>';
-            } elseif (strpos($firstDay, 'jeudi') !== false) {
-                echo '<div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div>';
-            } elseif (strpos($firstDay, 'vendredi') !== false) {
-                echo '<div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div>';
-            } elseif (strpos($firstDay, 'samedi') !== false) {
-                echo '<div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div>';
-            } elseif (strpos($firstDay, 'dimanche') !== false) {
-                echo '<div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div>';
-            }
+        if (strpos($firstDay, 'mardi') !== false) {
+            echo '<div class="emptyCell text-center"></div>';
+        } elseif (strpos($firstDay, 'mercredi') !== false) {
+            echo '<div class="emptyCell text-center"></div><div class="emptyCell text-center"></div>';
+        } elseif (strpos($firstDay, 'jeudi') !== false) {
+            echo '<div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div>';
+        } elseif (strpos($firstDay, 'vendredi') !== false) {
+            echo '<div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div>';
+        } elseif (strpos($firstDay, 'samedi') !== false) {
+            echo '<div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div>';
+        } elseif (strpos($firstDay, 'dimanche') !== false) {
+            echo '<div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div><div class="emptyCell text-center"></div>';
+        }
 
-            foreach ($monthLength as $monthDays) {
-                echo '<div class="calendarCell text-center">' . $monthDays . '</div>';
-            }
+        foreach ($monthLength as $monthDays) {
+            echo '<div class="calendarCell text-center">' . $monthDays . '</div>';
         }
         ?>
     </div>
--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="fr" dir="ltr">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         <link href="assets/css/style.css" type="text/css" rel="stylesheet" />
         <title>PHP | Partie <?= $partNb ?> | <?= $exerciseNb ?></title>
@@ -10,7 +11,7 @@
         <div class="container bg-secondary">
             <h1 class="text-center text-dark">PHP | Partie <?= $partNb ?> | <?= $exerciseNb ?></h1>
             <div class="row bg-info">
-                <div class="col-2 bg-primary">
+                <div class="col-1 bg-primary">
                     <nav class="nav flex-column font-weight-bold">
                         <a class="nav-link text-warning" href="../Ex1/index.php">Ex1</a>
                         <a class="nav-link text-warning" href="../Ex2/index.php">Ex2</a>
@@ -23,4 +24,4 @@
                         <a class="nav-link text-warning" href="../TP/index.php">TP</a>
                     </nav>
                 </div>
-                <div id="exercicesContent" class="d-flex flex-column align-items-center justify-content-center col-10">
+                <div id="exercicesContent" class="ml-5 mb-3 mt-3 d-flex flex-column align-items-center justify-content-center col-10">
